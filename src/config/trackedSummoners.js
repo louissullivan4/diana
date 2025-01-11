40e9d3719e0c820437bd0e92f293b1413dbf9623
Louis Sullivan <sullivanlouis0@gmail.com>
2025-01-11 03:33:37 +0000

    Updated ignore

--- src/config/trackedSummoners.js
+++ src/config/trackedSummoners.js
@@ -1,12 +1,13 @@
 // config/trackedSummoners.js
 const { Constants } = require('twisted');
+require('dotenv').config();
 
 const trackedSummoners = [
   {
     puuid: '01XS0CtuZCXjlubxeCMW3RFZTS7WAo38zHvFWeokv3rGK6XDYVW4pPG586vKrKdccA3Ru2KA0OtQag',
     summonerName: 'YngStew',
     tagLine: '1495',
-    discordChannelId: '1263016855455531021',
+    discordChannelId: process.env.DISCORD_CHANNEL_ID,
     regionGroup: Constants.RegionGroups.EUROPE,
     matchRegionPrefix: 'EUW1',
   },
@@ -14,7 +15,7 @@ const trackedSummoners = [
     puuid: 'ywqK5bySVAUGZXGcDAZns5wSZkKSL2gUA3_wBZQ57VdMm5UbeTNrN3J1YNaH97CACg7pe5g0oxkGdA',
     summonerName: 'I miss her',
     tagLine: 'Ascd',
-    discordChannelId: '1263016855455531021',
+    discordChannelId: process.env.DISCORD_CHANNEL_ID,
     regionGroup: Constants.RegionGroups.EUROPE,
     matchRegionPrefix: 'EUW1',
   },
