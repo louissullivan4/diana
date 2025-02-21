d0e2d9d2b5806df94b01a95e3fb058a62da0bf13
Louis Sullivan <sullivanlouis0@gmail.com>
2025-02-21 19:37:12 +0000

    Add yez

---
+++ backend/src/config/trackedSummoners.js
@@ -0,0 +1,19 @@
+const trackedSummoners = [
+  {
+    puuid: '01XS0CtuZCXjlubxeCMW3RFZTS7WAo38zHvFWeokv3rGK6XDYVW4pPG586vKrKdccA3Ru2KA0OtQag'
+  },
+  {
+    puuid: 'ywqK5bySVAUGZXGcDAZns5wSZkKSL2gUA3_wBZQ57VdMm5UbeTNrN3J1YNaH97CACg7pe5g0oxkGdA',
+  },
+  {
+    puuid: 'rKCZwiocEjGVQp7pLq9WkrxhN5VGddJh-TX3NRa7LXslIZsWygt2r3UXdfg2DSwoY4Q3NpKTcbrkWg',
+  },
+  {
+    puuid: 'wsedDXW_ao6vCxfpdxBXohBYLZghQMtg7YxWtnVo9p_H3iv-M7ibWM2fU7MkK6Vn8LgXTOCLveex7w',
+  },
+  {
+    puuid: 'VcKXZg_8LUk4vkqL_M93rKfOQCqzmTAMFIoJ7SwLXv56zpDOl2gsN9z05R8iFqwHx',
+  }
+];
+
+module.exports = trackedSummoners;
